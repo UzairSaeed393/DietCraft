90c00cbbbd2e1138c7b7b9401e7a1e7911b93deb
UzairSaeed393 <saeeduzair393@gmail.com>
2026-02-17 12:36:13 +0500

    change2

--- nutritionists/models.py
+++ nutritionists/models.py
@@ -12,28 +12,22 @@ class Nutritionist(models.Model):
     name = models.CharField(max_length=100)
     specialty = models.CharField(max_length=200)
     category = models.CharField(
-        max_length=20,
-        choices=CATEGORY_CHOICES,
-        default="general"
+        max_length=20,choices=CATEGORY_CHOICES,default="general"
     )
 
     clinic_name = models.CharField(max_length=200)
     clinic_address = models.CharField(max_length=300)
     city = models.CharField(
-        max_length=20,
-        help_text="e.g. Lahore, Gujrat, Islamabad"
+        max_length=20,help_text="e.g. Lahore, Gujrat, Islamabad"
     )
-    experience_years = models.PositiveIntegerField(default=1)
 
+    experience_years = models.PositiveIntegerField(default=1)
     rating = models.DecimalField(
-        max_digits=2,
-        decimal_places=1,
-        default=3.5
+        max_digits=2,decimal_places=1,default=3.5
     )
 
     timing = models.CharField(
-        max_length=50,
-        help_text="e.g. 1:00 PM – 3:00 PM"
+        max_length=50,help_text="e.g. 1:00 PM – 3:00 PM"
     )
 
     degrees = models.CharField(
@@ -42,9 +36,7 @@ class Nutritionist(models.Model):
     )
 
     image = models.ImageField(
-        upload_to="nutritionists/",
-        null=True,
-        blank=True
+        upload_to="nutritionists/",null=True,blank=True
     )
 
     def __str__(self):
